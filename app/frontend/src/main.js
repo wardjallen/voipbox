7919c488d4f54050d08aeb9164e419eb6521e98e
Jeff Ward <jward@pedestaltechnology.com>
2021-12-01 16:53:05 -0500

    updated docker

--- app/frontend/src/main.js
+++ app/frontend/src/main.js
@@ -1,6 +1,11 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import axios from 'axios';
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+
+axios.defaults.withCredentials = true
+axios.defaults.baseURL = "http://localhost:8000" 
 
 Vue.config.productionTip = false
 
