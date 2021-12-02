571cca3a9a2eaec9496ad3c2d7911d221d1d9e9f
Jeff Ward <jward@pedestaltechnology.com>
2021-12-02 11:34:52 -0500

    added db

--- app/backend/src/main.py
+++ app/backend/src/main.py
@@ -1,6 +1,10 @@
 import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from tortoise.contrib.fastapi import HTTPNotFoundError, register_tortoise
+
+from src.database.register import register_tortoise
+from src.database.config import TORTOISE_ORM
 
 
 app = FastAPI()
@@ -17,7 +21,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-
+register_tortoise(app, config=TORTOISE_ORM, generate_schemas=True)
 
 
 
